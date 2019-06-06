730a9ae142680352ff72aa7ea9a59f2127b7b370
AndreiAniukou <antei1231@gmail.com>
2019-06-07 00:49:45 +0300

    Initial commit

---
+++ blog/blg/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def posts_list(request):
+    n = ['Andrei', 'ellie', 'tanya']
+    return render(request, 'blg/index.html', context={'names': n})
