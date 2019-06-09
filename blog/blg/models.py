99af8cdc7b13585ac69a3f989dda25f469cfd027
AndreiAniukou <antei1231@gmail.com>
2019-06-10 00:08:43 +0300

    upd1

--- blog/blg/models.py
+++ blog/blg/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.shortcuts import reverse
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)#db_index для быстрого поиска
+    slug = models.SlugField(max_length=150, unique=True)#разрешает использовать все символы и уникальные индификатры
+    body = models.TextField(blank=True, db_index=True)#blanc чтобы поле было пустым разрешить
+    date_pub = models.DateTimeField(auto_now_add=True)#auto_now ставит дату при сохранении в бд
+
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug}) #генерирует ссылку
+
+
+    def __str__(self):
+        return '{}'.format(self.title)#для вывода
--- blog/blg/views.py
+++ blog/blg/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 def posts_list(request):
-    n = ['Andrei', 'ellie', 'tanya']
-    return render(request, 'blg/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blg/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug_iexact=slug) #получает из бд ссылку
+    render(request, 'blog/post_detail.html', context={'post': post})
